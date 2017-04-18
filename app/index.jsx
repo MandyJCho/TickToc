042a6da980ce6a85db3b05024a433f6e21ec30f7
MandyJCho <chomandy96@gmail.com>
2017-04-18 16:14:59 -0500

    Test component up

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./styles/base";
+
+function Comp(){
+  return (
+      <div>Hi</div>
+  );
+}
+
+ReactDOM.render(<Comp/>, document.getElementById('app'));
