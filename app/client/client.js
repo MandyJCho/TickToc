5654371766cfb10e557ec76682fc1ebc02e6a948
MandyJCho <chomandy96@gmail.com>
2017-05-25 15:23:12 -0500

    Use axios over fetch

---
+++ app/client/client.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import config from './config';
+
+module.exports = (function () {
+  // GET methods
+  function getTimers() {
+    axios.get();
+  }
+
+  return {
+    getTimers,
+  };
+}());
